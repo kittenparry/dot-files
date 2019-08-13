ad202122d43c93da2bee37339b9587c87f0d2b3b
Edvin Boul <kittenparry@users.noreply.github.com>
2019-08-13 22:58:44 +0300

    Switch from Chrome to Opera as main browser
    Use $BROWSER instead of static values in several places
    Remove some comments in i3 config
    Move Opera to ws1
    In reddit notifications script get window class dynamically*
    Opera icon to ws1, icon of ws5 is default now

--- .config/polybar/scripts/notification-reddit.sh
+++ .config/polybar/scripts/notification-reddit.sh
@@ -9,11 +9,18 @@ USERAGENT="polybar-scripts/notification-reddit:v1.0 u/username"
 
 notifications=$(curl -sf --user-agent "$USERAGENT" "$URL" | jq '.["data"]["children"] | length')
 
+cls=''
+if [[ $BROWSER =~ 'opera$' ]]; then
+	cls='Opera'
+elif [[ $BROWSER =~ 'google-chrome-stable$' ]]; then
+	cls='Google-chrome'
+fi
+
 if [ -n "$notifications" ] && [ "$notifications" -gt 0 ]; then
 	# reddit-alien in reddit's colour
 	# open chrome on unread messages and switch to ws1
-	echo "%{A1:google-chrome-stable https\://www.reddit.com/message/unread/ ; i3-msg '[class=\"Google-chrome\"]' focus:}%{F#ff4500} $notifications%{F-}%{A}"
+	echo "%{A1:$BROWSER https\://www.reddit.com/message/unread/ ; i3-msg '[class=\"$cls\"]' focus:}%{F#ff4500} $notifications%{F-}%{A}"
 else
 	# open reddit and switch to ws1
-	echo "%{A1:google-chrome-stable https\://www.reddit.com/ ; i3-msg '[class=\"Google-chrome\"]' focus:}%{A}"
+	echo "%{A1:$BROWSER https\://www.reddit.com/ ; i3-msg '[class=\"$cls\"]' focus:}%{A}"
 fi
